1743344c411b9ec0b8f9a1a857f26dd6a547fd28
Derek <dwarren4096@gmail.com>
2015-08-18 21:03:31 -0700

    Revert "changing up search algorith"
    This reverts commit f4b3e5c99daa8661a2e5936bb2cbde1a34b6a8de.

--- search.py
+++ search.py
@@ -15,18 +15,6 @@ def HTMLSearch(path):
   include = r'|'.join([fnmatch.translate(x) for x in include])
   #exclude = r'|'.join([fnmatch.translate(x) for x in exclude])
   
-  files = search(path, include, exclude)
-
-  return files
-
-def blogSearch(path):
-  blogRoot = path + '/blog'
-  #grab only .html files
-  include = ['*.html']
-  #directories to exclude
-  exclude = ["_notes", "assets_c"]
-
-def search(path, include, exclude):
   for root, dirs, files in os.walk(path, topdown=True):
     dirs[:] = [d for d in dirs if d not in exclude]
     
@@ -34,9 +22,10 @@ def search(path, include, exclude):
     #for d in dirs:
     #  print ("Dirs:", d)
     
-    files = [os.path.join(root, f) for f in files]	# appends path to filename
-    files = [f for f in files if re.match(include, f)]	# filters anything that isn't an .html file
+    files = [os.path.join(root, f) for f in files]
+    files = [f for f in files if re.match(include, f)]
     for fname in files:
       print (fname)
-    
+
   return files
+
